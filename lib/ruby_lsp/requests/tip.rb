3ebc7f32d53a6db33a855049bcfbafb226b43404
Mads Ohm Larsen <mads.larsen@shopify.com>
2023-02-09 15:25:26 +0100

    Use search and match patterns to find code to apply tips

--- lib/ruby_lsp/requests/tip.rb
+++ lib/ruby_lsp/requests/tip.rb
@@ -23,61 +23,68 @@ module RubyLsp
       def run
         return if @document.syntax_error?
 
-        visit(@document.tree)
+        TIPS.each do |key, tip|
+          next unless tip["search_pattern"]
+
+          SyntaxTree.search(@document.source, tip["search_pattern"]) do |node|
+            range = range_from_methods(tip.fetch("range", []), node)
+            next if range.nil?
+
+            template_variables = tip.fetch("extraction_methods", []).each_with_object({}) do |(name, chain), hash|
+              matched_node = run_method_chain(chain, node)
+              hash[name.to_sym] = node_name(matched_node)
+            end
+
+            message = format(tip["message"], template_variables)
+            text = replacement_text(range)
+
+            add_tip(range, text, tip["code"], message)
+          end
+        rescue StandardError => e
+          warn("Failed to parse tip: #{key}: #{e}")
+        end
+
         @tips
       end
 
-      sig { params(node: SyntaxTree::ClassDeclaration).void }
-      def visit_class(node)
-        if node.superclass
-          range = range_between_nodes(node.constant, node.superclass)
-          class_name = node_name(node.constant)
-          super_class_name = node_name(node.superclass)
-          code, message = fetch_tip("superclass", { class_name: class_name, super_class_name: super_class_name })
-
-          add_tip(range, "#{class_name} < #{super_class_name}", code, message)
-        end
-
-        super
-      end
-
-      sig { params(node: SyntaxTree::Command).void }
-      def visit_command(node)
-        range = range_from_syntax_tree_node(node.message)
-        value = node.message.value
-
-        case value
-        when "validate"
-          method_name = node_name(node.arguments.parts.first)
-          code, message = fetch_tip("validate", { method_name: method_name })
-
-          add_tip(range, value, code, message)
-        when "validates"
-          field_name = node_name(node.arguments.parts.first)
-          code, message = fetch_tip("validates", { field_name: field_name })
-
-          add_tip(range, value, code, message)
-        end
-
-        super
-      end
-
-      sig { params(node: SyntaxTree::DefNode).void }
-      def visit_def(node)
-        method_name = node_name(node)
-
-        case method_name
-        when "method_missing"
-          range = range_from_syntax_tree_node(node.name)
-          code, message = fetch_tip("method_missing")
-
-          add_tip(range, method_name, code, message)
-        end
-
-        super
-      end
-
       private
+
+      sig { params(range: Interface::Range).returns(String) }
+      def replacement_text(range)
+        lines = @document.source.split("\n").map(&:chars)
+        value = T.must(lines[range.start.line..range.end.line])
+        (T.must(value[-1]).size - range.end.character).times { T.must(value[-1]).pop }
+        range.start.character.times { T.must(value[0]).shift }
+        value.map(&:join).join("\n")
+      end
+
+      sig { params(range_methods: T::Array[String], node: SyntaxTree::Node).returns(T.nilable(Interface::Range)) }
+      def range_from_methods(range_methods, node)
+        return if range_methods.empty?
+        return unless [1, 2].include?(range_methods.size)
+
+        method_chain_from, method_chain_to = range_methods
+
+        if range_methods.size == 1
+          matched_node = run_method_chain(method_chain_from, node)
+
+          range_from_syntax_tree_node(matched_node)
+        else
+          from_matched_node = run_method_chain(method_chain_from, node)
+          to_matched_node = run_method_chain(method_chain_to, node)
+
+          range_between_nodes(from_matched_node, to_matched_node)
+        end
+      end
+
+      sig { params(methods: T.nilable(String), node: SyntaxTree::Node).returns(SyntaxTree::Node) }
+      def run_method_chain(methods, node)
+        return node if methods.nil?
+
+        methods.split(".").reduce(node) do |acc, method|
+          acc.public_send(method)
+        end
+      end
 
       sig do
         params(range: Interface::Range, value: T.nilable(String), code: T.any(Integer, String), message: String).void
@@ -112,17 +119,8 @@ module RubyLsp
       end
 
       sig do
-        params(key: String, replacements: T.nilable(T::Hash[String, String])).returns([Integer, String])
-      end
-      def fetch_tip(key, replacements = nil)
-        [
-          TIPS.dig(key, "code"),
-          format(TIPS.dig(key, "message"), replacements),
-        ]
-      end
-
-      sig do
         params(node: T.nilable(T.any(
+          SyntaxTree::Node,
           SyntaxTree::VarRef,
           SyntaxTree::ConstRef,
           SyntaxTree::DefNode,
@@ -154,6 +152,8 @@ module RubyLsp
           node.value
         when Symbol
           node.to_s
+        when SyntaxTree::Node
+          # SyntaxTree::Node is a superclass of all the above.
         else
           T.absurd(node)
         end
