84f9657bfbd3d9b2b7df6d00f31fdd345aa92fa3
Jenny Shih <jenny.shih@shopify.com>
2023-02-13 10:41:57 -0500

    Update local path to ruby_test_runner and add support for block-style test declarations
    Co-authored-by: Andy Waite <andy.waite@shopify.com>

--- lib/ruby_lsp/requests/code_lens.rb
+++ lib/ruby_lsp/requests/code_lens.rb
@@ -36,47 +36,52 @@ module RubyLsp
         @code_lenses
       end
 
-      sig { override.params(node: SyntaxTree::DefNode).void }
-      def visit_def(node)
-        super
-        name = node.name.value
-        return unless name.start_with?("test_")
-
-        @code_lenses << code_lens_for_single_test(node, name)
-      end
-
       sig { override.params(node: SyntaxTree::ClassDeclaration).void }
       def visit_class(node)
         super
         name = node.constant.constant.value
         return unless name.end_with?("Test")
 
-        @code_lenses << code_lens_for_all_tests_in_file(node)
+        path = @uri.delete_prefix("file://#{Dir.pwd}/")
+        command = RubyTestRunner::Command.new(Dir.pwd, path, nil).runner_command
+        @code_lenses << code_lens_to_run_test(
+          "✨ Run all tests in this file ✨", range_from_syntax_tree_node(node), command
+        )
+      end
+
+      sig { override.params(node: SyntaxTree::DefNode).void }
+      def visit_def(node)
+        super
+        name = node.name.value
+        return unless name.start_with?("test_")
+
+        path = @uri.delete_prefix("file://#{Dir.pwd}/")
+        command = RubyTestRunner::Command.new(Dir.pwd, path, nil).runner_command
+        @code_lenses << code_lens_to_run_test(
+          "🔍 Run this test", range_from_syntax_tree_node(node), command
+        )
+      end
+
+      sig { override.params(node: SyntaxTree::Command).void }
+      def visit_command(node)
+        warn(node.message.value)
+        return unless ["test", "it"].include?(node.message.value)
+
+        path = @uri.delete_prefix("file://#{Dir.pwd}/")
+        command = RubyTestRunner::Command.new(Dir.pwd, path, nil).runner_command
+        @code_lenses << code_lens_to_run_test(
+          "🔍 Run this test", range_from_syntax_tree_node(node), command
+        )
       end
 
       private
 
-      sig { params(node: SyntaxTree::DefNode, name: String).returns(Interface::CodeLens) }
-      def code_lens_for_single_test(node, name)
-        path = @uri.delete_prefix("file://")
-
+      sig { params(title: String, range: Interface::Range, command: String).returns(Interface::CodeLens) }
+      def code_lens_to_run_test(title, range, command)
         Interface::CodeLens.new(
-          range: range_from_syntax_tree_node(node),
+          range: range,
           command: Interface::Command.new(
-            command: RUN_TEST_COMMAND, title: "🔍 Run this test", arguments: [path, name],
-          ),
-        )
-      end
-
-      sig { params(node: SyntaxTree::ClassDeclaration).returns(Interface::CodeLens) }
-      def code_lens_for_all_tests_in_file(node)
-        path = @uri.delete_prefix("file://#{Dir.pwd}/")
-        command = RubyTestRunner::Command.new(Dir.pwd, path, nil).runner_command
-
-        Interface::CodeLens.new(
-          range: range_from_syntax_tree_node(node),
-          command: Interface::Command.new(
-            command: RUN_TEST_COMMAND, title: "✨ Run all tests in this file ✨", arguments: [command],
+            command: RUN_TEST_COMMAND, title: title, arguments: [command],
           ),
         )
       end
